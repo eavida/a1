e90be1c1f48aed3fbc349aeea3131d2475a88c51
eavida <marcosjr268@gmail.com>
2024-11-22 20:16:46 -0300

    Build 0.2

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+096481753ebfcc2cddf0895ca6ba540b093d43aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d912103c9d6d93745cfb1fdb98dbaa0e458bfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
